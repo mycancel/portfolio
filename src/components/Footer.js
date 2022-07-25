e7801389f7a6d2b1462affbd7641bf0321cf2258
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-07-25 09:52:44 -0400

    added css to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <footer>
-      <a href="https://github.com/mycancel">Github</a>
-      <a href="https://www.linkedin.com/in/mary-cance/">LinkedIn</a>
-      <a href="https://stackoverflow.com/users/18851472/mycancel">Stack Overflow</a>
+    <footer className="text-center p-3">
+      <a
+        className="m-4"
+        href="https://github.com/mycancel"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Github
+      </a>
+      <a
+        className="m-4"
+        href="https://stackoverflow.com/users/18851472/mycancel"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Stack Overflow
+      </a>
+      <a
+        className="m-4"
+        href="https://www.linkedin.com/in/mary-cance/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        LinkedIn
+      </a>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
